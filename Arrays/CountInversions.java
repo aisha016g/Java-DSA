e5e4396ba811daa8b1959b22abd753630b519dcc
Aisha Gupta <aisha016g@gmail.com>
2026-04-06 19:11:27 +0530

    Day 13

---
+++ Arrays/CountInversions.java
@@ -0,0 +1,32 @@
+import java.util.*;
+public class CountInversions {
+
+    public static int Brute(int[] arr) {
+        int n = arr.length;
+        int count = 0;
+
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (arr[i] > arr[j]) {
+                    count++;
+                }
+            }
+        }
+
+        return count;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+
+        for(int i = 0; i < n; i++){
+            arr[i] = sc.nextInt();
+        }
+        int result = Brute(arr);
+        System.out.println(result);
+        sc.close();
+    }
+}
