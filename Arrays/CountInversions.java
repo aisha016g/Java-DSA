a232a24e11fa6b943aa1b35846f9fcf4e1b68f85
Aisha Gupta <aisha016g@gmail.com>
2026-04-08 20:44:51 +0530

    using merge sort optimal solution

--- Arrays/CountInversions.java
+++ Arrays/CountInversions.java
@@ -12,8 +12,53 @@ public class CountInversions {
                 }
             }
         }
-
         return count;
+    }
+    //optimal
+    public int mergeS(int[] arr, int low, int high) {
+        if (low >= high) return 0;
+
+        int mid = (low + high) / 2;
+
+        int left=mergeS(arr, low, mid);
+        int right=mergeS(arr, mid + 1, high);
+        int mergeCount=merge(arr, low, mid, high);
+        return left+right+mergeCount;
+    }
+
+    public int merge(int[] arr, int low, int mid, int high) {
+        int[] temp = new int[high - low + 1];
+
+        int left = low;
+        int right = mid + 1;
+        int idx = 0;
+        int cnt=0;
+        // merge two sorted halves
+        while (left <= mid && right <= high) {
+            if (arr[left] <= arr[right]) {
+                temp[idx++] = arr[left++];
+            } else {
+                temp[idx++] = arr[right++];
+                // Counting Inversions
+                cnt+=(mid-left+1);
+            }
+        }
+
+        // remaining left
+        while (left <= mid) {
+            temp[idx++] = arr[left++];
+        }
+
+        // remaining right
+        while (right <= high) {
+            temp[idx++] = arr[right++];
+        }
+
+        // copy back to original array
+        for (int i = 0; i < temp.length; i++) {
+            arr[low + i] = temp[i];
+        }
+        return cnt;
     }
 
     public static void main(String[] args) {
@@ -25,7 +70,9 @@ public class CountInversions {
         for(int i = 0; i < n; i++){
             arr[i] = sc.nextInt();
         }
-        int result = Brute(arr);
+        CountInversions sol=new CountInversions();
+        // int result = sol.Brute(arr);
+        int result = sol.mergeS(arr,0,n-1);
         System.out.println(result);
         sc.close();
     }
