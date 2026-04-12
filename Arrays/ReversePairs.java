71ff3e91109ef9206bed6d63a46eedfec0aed4f2
Aisha Gupta <aisha016g@gmail.com>
2026-04-12 11:27:14 +0530

    changed codes

--- Arrays/ReversePairs.java
+++ Arrays/ReversePairs.java
@@ -15,6 +15,49 @@ public class ReversePairs {
 
         return count;
     }
+    public int mergeS(int [] arr,int low,int high){
+        if(low>=high)return 0;
+        int mid=(low+high)/2;
+        int cnt=0;
+        cnt+=mergeS(arr,low,mid);
+        cnt+=mergeS(arr,mid+1,high);
+        cnt+=CountPairs(arr,low,mid,high);
+        merge(arr,low,mid,high);
+        return cnt;
+    }
+    public int CountPairs(int[] arr,int low,int mid,int high){
+        int right=mid+1,cnt=0;
+        for(int i=low;i<=mid;i++){
+            while(right<=high && arr[i]>2L*arr[right]){
+                cnt+=(right-(mid+1));
+            }
+        }
+        return cnt;
+    }
+    public void merge(int[] arr,int low,int mid,int high){
+        int left=low;
+        int right=mid+1;
+        int idx=0;
+        int[] temp=new int[high-low+1];
+        while(left<=mid && right<=high){
+            if(arr[left]<=arr[right])
+            {
+                temp[idx++]=arr[left++];
+            }
+            else{
+                temp[idx++]=arr[right++];
+            }
+        }
+        while(left<=mid){
+            temp[idx++]=arr[left++];
+        }
+        while(right<=high){
+            temp[idx++]=arr[right++];
+        }
+        for(int i=0;i<temp.length;i++){
+            arr[low+i]=temp[i];
+        }
+    }
 
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
@@ -24,9 +67,9 @@ public class ReversePairs {
         {
             arr[i]=sc.nextInt();
         }
-        // int res=ReversePairs.Better(arr);
+        ReversePairs sol=new ReversePairs();
         // int res=ReversePairs.Brute(arr);
-        int res=ReversePairs.Brute(arr);
+        int res=sol.mergeS(arr,0,n-1);
         System.out.println(res);
         sc.close();
     }
---
+++ BinarySearch/firstAndLastOccur.java
@@ -0,0 +1,69 @@
+import java.util.*;
+public class firstAndLastOccur {
+    public void Brute(int[] arr,int key){
+        int n=arr.length;
+        int first=-1,last=-1;
+        for(int i=0;i<n;i++){
+            if(arr[i]==key)
+            {
+                if(first==-1){
+                    first=i;
+                }
+                last=i;
+            }
+            
+        }
+        System.out.println(first+" "+last);
+    }
+    public int lower(int[] arr, int n, int x) {
+        int ans = n;
+        int low = 0, high = n - 1;
+
+        while (low <= high) {
+            int mid = (low + high) / 2;
+
+            if (arr[mid] >= x) {
+                ans = mid;
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+        return ans;
+    }
+    // UPPER BOUND → first index where arr[i] > x
+    public int upper(int[] arr, int n, int x) {
+        int ans = n;
+        int low = 0, high = n - 1;
+
+        while (low <= high) {
+            int mid = (low + high) / 2;
+
+            if (arr[mid] > x) {
+                ans = mid;
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+        return ans;
+    }
+    pair<int,int>firstLastPos(vector<int>arr,int n,int k)
+    {
+        int lb=lower
+    }
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int[]arr=new int[n];
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+        }
+        System.out.println("Enter the key");
+        int key=sc.nextInt();
+        firstAndLastOccur sol=new firstAndLastOccur();
+        sol.Brute(arr,key);
+        sc.close();
+    }
+    
+}
