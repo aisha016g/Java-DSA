3b3b757b81f6b071aea7943b16791a1dc8dd8072
Aisha Gupta <aisha016g@gmail.com>
2026-04-18 19:05:45 +0530

    updated

--- Arrays/zero_ones_twos.java
+++ Arrays/zero_ones_twos.java
@@ -3,14 +3,14 @@ import java.util.*;
 public class zero_ones_twos {
     public int[] Brute(int[] arr){
         int n=arr.length;
-        int cnt0=0,cnt1=0,cnt2=0;
+        int cnt0=0,cnt1=0;
         for(int i=0;i<n;i++){
             if(arr[i]==0)
                 cnt0++;
             else if(arr[i]==1)
                 cnt1++;
             else
-                cnt2++;
+                continue;
         }
         for(int i=0;i<cnt0;i++){
             arr[i]=0;
