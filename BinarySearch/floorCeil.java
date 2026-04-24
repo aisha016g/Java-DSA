c0f331f132a0279e31509d935d044ec7ee74918b
Aisha Gupta <aisha016g@gmail.com>
2026-04-24 20:51:16 +0530

    Day 31

---
+++ BinarySearch/floorCeil.java
@@ -0,0 +1,62 @@
+import java.util.*;
+public class floorCeil {
+
+    // Function to find floor
+    public int findFloor(int[] arr, int x) {
+        int low = 0, high = arr.length - 1;
+        int ans = -1;
+
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            if (arr[mid] <= x) {
+                ans = arr[mid];     // Potential floor
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+        return ans;
+    }
+
+    // Function to find ceiling
+    public int findCeil(int[] arr, int x) {
+        int low = 0, high = arr.length - 1;
+        int ans = -1;
+
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            if (arr[mid] >= x) {
+                ans = arr[mid];     // Potential ceil
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+        return ans;
+    }
+
+    // Function to return floor and ceil as array
+    public int[] getFloorAndCeil(int[] arr, int x) {
+        int f = findFloor(arr, x);
+        int c = findCeil(arr, x);
+        return new int[]{f, c};
+    }
+
+    public static void main(String[] args) {
+         Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int[] arr=new int[n];
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+        } 
+        System.out.println("Enter the key");
+        int x=sc.nextInt();
+        
+        floorCeil finder = new floorCeil();
+        int[] res = finder.getFloorAndCeil(arr, x);
+        System.out.println("The floor and ceil are: " + res[0] + " " + res[1]);
+        sc.close();
+    }
+}
+
+    
