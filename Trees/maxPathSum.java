ed20379f35965927bbdb31ab13ade858d7606f47
Aisha Gupta <aisha016g@gmail.com>
2026-05-22 20:49:58 +0530

    changes

--- Trees/maxPathSum.java
+++ Trees/maxPathSum.java
@@ -1,75 +1,26 @@
+import java.util.*;
+class node{
+    int data;
+    node left;
+    node right;
+node(int val){
+    data=val;
+    left=right=null;
+}
+}
+class Sol{
+    public int brute(node root){
+        
+    }
+}
+
 class maxPathSum {
-
-    // Definition for binary tree node
-    static class TreeNode {
-        int val;
-        TreeNode left;
-        TreeNode right;
-
-        TreeNode(int val) {
-            this.val = val;
-            left = null;
-            right = null;
-        }
-    }
-
-    // Global variable to store maximum answer
-    int maxSum = Integer.MIN_VALUE;
-
-    // Function to calculate maximum gain
-    public int maxGain(TreeNode root) {
-
-        // Base case
-        if(root == null)
-            return 0;
-
-        // Find left gain
-        int left = Math.max(0, maxGain(root.left));
-
-        // Find right gain
-        int right = Math.max(0, maxGain(root.right));
-
-        // Current path sum
-        int currentPath = root.val + left + right;
-
-        // Update maximum answer
-        maxSum = Math.max(maxSum, currentPath);
-
-        // Return maximum gain to parent
-        return root.val + Math.max(left, right);
-    }
-
-    // Main function
-    public int maxSum(TreeNode root) {
-
-        maxGain(root);
-
-        return maxSum;
-    }
-
-    // Main method
     public static void main(String[] args) {
+        node root=new node(1);
+        root.left=new node(2);
+        root.right=new node(3);
+        root.right.left=new node(4);
+        root.right.right=new node(5);
 
-        maxPathSum obj = new maxPathSum();
-
-        /*
-                -10
-                /  \
-               9   20
-                  /  \
-                 15   7
-        */
-
-        TreeNode root = new TreeNode(-10);
-
-        root.left = new TreeNode(9);
-
-        root.right = new TreeNode(20);
-        root.right.left = new TreeNode(15);
-        root.right.right = new TreeNode(7);
-
-        int ans = obj.maxSum(root);
-
-        System.out.println("Maximum Path Sum = " + ans);
     }
 }
