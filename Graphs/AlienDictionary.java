083d48311b2b04d00f385c95f81bd0c3fc5a7869
Aisha Gupta <aisha016g@gmail.com>
2026-06-24 00:32:03 +0530

    completed

--- Graphs/AlienDictionary.java
+++ Graphs/AlienDictionary.java
@@ -1,34 +1,82 @@
 import java.util.*;
 
-class Alien{
-    public String findOrder(String[] dict,int N,int K){
+class Alien {
+    public String findOrder(String[] dict, int N, int K){
         ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
 
-        for(int i=0;i<K;i++){
+        for (int i = 0; i < K; i++) {
             adj.add(new ArrayList<>());
         }
 
-        for(int i=0;i<N-1;i++){
+        // Build graph
+        for (int i = 0; i < N - 1; i++) {
             String s1 = dict[i];
-            String s2 = dict[i+1];
-            int len = Math.min(s1.length(),s2.length());
+            String s2 = dict[i + 1];
 
-            for(int ptr=0;ptr<len;ptr++){
-                if(s1.charAt(ptr) != s2.charAt(ptr)){
+            int len = Math.min(s1.length(), s2.length());
+            for (int ptr = 0; ptr < len; ptr++) {
+                if (s1.charAt(ptr) != s2.charAt(ptr)) {
                     adj.get(s1.charAt(ptr) - 'a').add(s2.charAt(ptr) - 'a');
                     break;
                 }
             }
         }
-        List<Integer> topo = topoSort(K,adj);
+
+        List<Integer> topo = topoSort(K, adj);
         String ans = "";
-        for(int it:topo){
-            ans = ans + (char)(it+(int)('a'));
+        for (int it : topo) {
+            ans += (char) (it + 'a');
         }
+
         return ans;
+    }
+
+    public List<Integer> topoSort(int V, ArrayList<ArrayList<Integer>> adj) {
+        int[] indegree = new int[V];
+
+        for (int i = 0; i < V; i++) {
+            for (int it : adj.get(i)) {
+                indegree[it]++;
+            }
+        }
+
+        Queue<Integer> q = new LinkedList<>();
+
+        for (int i = 0; i < V; i++) {
+            if (indegree[i] == 0) {
+                q.offer(i);
+            }
+        }
+
+        List<Integer> topo = new ArrayList<>();
+
+        while (!q.isEmpty()) {
+            int node = q.poll();
+            topo.add(node);
+
+            for (int it : adj.get(node)) {
+                indegree[it]--;
+                if (indegree[it] == 0) {
+                    q.offer(it);
+                }
+            }
+        }
+
+        return topo;
     }
 }
 
 public class AlienDictionary {
-    
+    public static void main(String[] args) {
+        String[] dict = {"baa","abcd","abca","cab","cad"};
+
+        int N = dict.length;
+        int K = 4; // a, b, c, d
+
+        Alien obj = new Alien();
+
+        String order = obj.findOrder(dict, N, K);
+
+        System.out.println("Alien Dictionary Order: " + order);
+    }
 }
